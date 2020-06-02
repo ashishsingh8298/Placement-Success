03786a84f1adb7a70f706d999827c840e1eb0d30
Aprajita <aprajitashailie88@gmail.com>
2020-06-02 15:47:54 +0530

    whats new section added

---
+++ app/src/main/java/com/example/basic/whatsNew.java
@@ -0,0 +1,14 @@
+package com.example.basic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class whatsNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whats_new);
+    }
+}
