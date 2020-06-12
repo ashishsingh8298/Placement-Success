b1c1641caf47beba7a5b3dd2fa0a1966e49019ce
Ashish SIngh <ashsihs41@gmail.com>
2020-06-12 14:57:54 +0530

    Update

--- app/src/main/java/com/example/basic/noJobsPosted.java
+++ app/src/main/java/com/example/basic/noJobsPosted.java
@@ -2,13 +2,23 @@ package com.example.basic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class noJobsPosted extends AppCompatActivity {
-
+Button feed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_jobs_posted);
+        feed=findViewById(R.id.buttonFeed);
+        feed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(noJobsPosted.this,Nav_Activity.class));
+            }
+        });
     }
 }
